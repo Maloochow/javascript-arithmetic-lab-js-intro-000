e31e82741b02d54a253651f49e6a8110c2f6b0c8
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-06 01:53:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ function subtract() {}
 function multiple() {}
 function divide() {}
 
-add(2,3)
-subtract(2, 3)
-multiple(2, 3)
-divide(2, 3)
+add(2,3) {
+  return 2 + 3
+}
+subtract(2, 3) {
+  return 2 - 3
+}
+multiple(2, 3) {
+  return 2 * 3
+}
+divide(2, 3) {
+  return 2 / 3
+}
