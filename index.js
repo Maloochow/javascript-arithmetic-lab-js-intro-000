40487c21acf86bbd04bc95cd14e5156a29406ff4
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-06 01:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ function divide() {}
 function add(a, b) {
   return a + b
 }
-subtract(a, b) {
+
+function subtract(a, b) {
   return a - b
 }
-multiple(a, b) {
+
+function multiple(a, b) {
   return a * b
 }
-divide(a, b) {
+
+function divide(a, b) {
   return a / b
 }
